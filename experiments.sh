84cea57670a682474d8efdff08d3627b2497dfcd
mache102 <bee365go@gmail.com>
2023-10-11 07:18:52 +0800

    add experiments sh, fix requirements

---
+++ experiments.sh
@@ -0,0 +1,3 @@
+python run_nerf.py --config configs/chair.txt --finest_res 512 --log2_hashmap_size 19 --lrate 0.01 --lrate_decay 10
+
+python run_nerf.py --config configs/st3d.txt --finest_res 512 --log2_hashmap_size 19 --lrate 0.01 --lrate_decay 10
