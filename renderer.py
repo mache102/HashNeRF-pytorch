e98be43cd2c3fee30fc7f8abaf5320650343fdbf
mache102 <bee365go@gmail.com>
2023-10-20 18:08:01 +0800

    convergence fix attempt 5: use original sample_pdf

--- renderer.py
+++ renderer.py
@@ -342,47 +342,92 @@ class VolumetricRenderer:
 
 
     # Hierarchical sampling (section 5.2)
-    def sample_pdf(self, bins, weights, det=False, eps=1e-5):
-        """
-        Sample N_samples samples from bins with distribution defined by weights.
-        Inputs:
-            bins: (N_rays, N_samples_+1) where N_samples_ is "the number of coarse samples per ray - 2"
-            weights: (N_rays, N_samples_)
-            N_samples: the number of samples to draw from the distribution
-            det: deterministic or not
-            eps: a small number to prevent division by zero
-        Outputs:
-            samples: the sampled samples
-        """
-        N_samples = self.N_importance 
+    # def sample_pdf(self, bins, weights, det=False, eps=1e-5):
+    #     """
+    #     Sample N_samples samples from bins with distribution defined by weights.
+    #     Inputs:
+    #         bins: (N_rays, N_samples_+1) where N_samples_ is "the number of coarse samples per ray - 2"
+    #         weights: (N_rays, N_samples_)
+    #         N_samples: the number of samples to draw from the distribution
+    #         det: deterministic or not
+    #         eps: a small number to prevent division by zero
+    #     Outputs:
+    #         samples: the sampled samples
+    #     """
+    #     N_samples = self.N_importance 
 
-        N_rays, N_samples_ = weights.shape
-        weights = weights + eps # prevent division by zero
-        pdf = weights / reduce(weights, 'n1 n2 -> n1 1', 'sum') # (N_rays, N_samples) (keep dims)
-        cdf = torch.cumsum(pdf, -1) # (N_rays, N_samples), cumulative distribution function
-        cdf = torch.cat([torch.zeros_like(cdf[: ,:1]), cdf], -1)  # (N_rays, N_samples_+1) 
-                                                                # padded to 0~1 inclusive
+    #     N_rays, N_samples_ = weights.shape
+    #     weights = weights + eps # prevent division by zero
+    #     pdf = weights / reduce(weights, 'n1 n2 -> n1 1', 'sum') # (N_rays, N_samples) (keep dims)
+    #     cdf = torch.cumsum(pdf, -1) # (N_rays, N_samples), cumulative distribution function
+    #     cdf = torch.cat([torch.zeros_like(cdf[: ,:1]), cdf], -1)  # (N_rays, N_samples_+1) 
+    #                                                             # padded to 0~1 inclusive
 
+    #     if det:
+    #         u = torch.linspace(0, 1, N_samples, device=bins.device)
+    #         u = u.expand(N_rays, N_samples)
+    #     else:
+    #         u = torch.rand(N_rays, N_samples, device=bins.device)
+    #     u = u.contiguous()
+
+    #     inds = torch.searchsorted(cdf, u, right=True)
+    #     below = torch.clamp_min(inds-1, 0)
+    #     above = torch.clamp_max(inds, N_samples_)
+
+    #     inds_sampled = rearrange(torch.stack([below, above], -1), 'n1 n2 c -> n1 (n2 c)', c=2)
+    #     cdf_g = rearrange(torch.gather(cdf, 1, inds_sampled), 'n1 (n2 c) -> n1 n2 c', c=2)
+    #     bins_g = rearrange(torch.gather(bins, 1, inds_sampled), 'n1 (n2 c) -> n1 n2 c', c=2)
+
+    #     denom = cdf_g[...,1]-cdf_g[...,0]
+    #     denom[denom<eps] = 1 # denom equals 0 means a bin has weight 0, in which case it will not be sampled
+    #                         # anyway, therefore any value for it is fine (set to 1 here)
+
+    #     samples = bins_g[...,0] + (u-cdf_g[...,0])/denom * (bins_g[...,1]-bins_g[...,0])
+    #     return samples
+
+    def sample_pdf(self, bins, weights, N_samples, det=False):
+        # Get pdf
+        weights = weights + 1e-5 # prevent nans
+        pdf = weights / torch.sum(weights, -1, keepdim=True)
+        cdf = torch.cumsum(pdf, -1)
+        cdf = torch.cat([torch.zeros_like(cdf[...,:1]), cdf], -1)  # (batch, len(bins))
+
+        # Take uniform samples
         if det:
-            u = torch.linspace(0, 1, N_samples, device=bins.device)
-            u = u.expand(N_rays, N_samples)
+            u = torch.linspace(0., 1., steps=N_samples)
+            u = u.expand(list(cdf.shape[:-1]) + [N_samples])
         else:
-            u = torch.rand(N_rays, N_samples, device=bins.device)
+            u = torch.rand(list(cdf.shape[:-1]) + [N_samples])
+
+        # Pytest, overwrite u with numpy's fixed random numbers
+        # if pytest:
+        #     np.random.seed(0)
+        #     new_shape = list(cdf.shape[:-1]) + [N_samples]
+        #     if det:
+        #         u = np.linspace(0., 1., N_samples)
+        #         u = np.broadcast_to(u, new_shape)
+        #     else:
+        #         u = np.random.rand(*new_shape)
+        #     u = torch.Tensor(u)
+
+        # Invert CDF
         u = u.contiguous()
-
         inds = torch.searchsorted(cdf, u, right=True)
-        below = torch.clamp_min(inds-1, 0)
-        above = torch.clamp_max(inds, N_samples_)
+        below = torch.max(torch.zeros_like(inds-1), inds-1)
+        above = torch.min((cdf.shape[-1]-1) * torch.ones_like(inds), inds)
+        inds_g = torch.stack([below, above], -1)  # (batch, N_samples, 2)
 
-        inds_sampled = rearrange(torch.stack([below, above], -1), 'n1 n2 c -> n1 (n2 c)', c=2)
-        cdf_g = rearrange(torch.gather(cdf, 1, inds_sampled), 'n1 (n2 c) -> n1 n2 c', c=2)
-        bins_g = rearrange(torch.gather(bins, 1, inds_sampled), 'n1 (n2 c) -> n1 n2 c', c=2)
+        # cdf_g = tf.gather(cdf, inds_g, axis=-1, batch_dims=len(inds_g.shape)-2)
+        # bins_g = tf.gather(bins, inds_g, axis=-1, batch_dims=len(inds_g.shape)-2)
+        matched_shape = [inds_g.shape[0], inds_g.shape[1], cdf.shape[-1]]
+        cdf_g = torch.gather(cdf.unsqueeze(1).expand(matched_shape), 2, inds_g)
+        bins_g = torch.gather(bins.unsqueeze(1).expand(matched_shape), 2, inds_g)
 
-        denom = cdf_g[...,1]-cdf_g[...,0]
-        denom[denom<eps] = 1 # denom equals 0 means a bin has weight 0, in which case it will not be sampled
-                            # anyway, therefore any value for it is fine (set to 1 here)
+        denom = (cdf_g[...,1]-cdf_g[...,0])
+        denom = torch.where(denom<1e-5, torch.ones_like(denom), denom)
+        t = (u-cdf_g[...,0])/denom
+        samples = bins_g[...,0] + t * (bins_g[...,1]-bins_g[...,0])
 
-        samples = bins_g[...,0] + (u-cdf_g[...,0])/denom * (bins_g[...,1]-bins_g[...,0])
         return samples
 
 if __name__ == '__main__':
